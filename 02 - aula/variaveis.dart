14b167d7e628d23ed7e4292433ab6723926d499c
Lessa Apolinario <lessaapolinario22@outlook.com>
2021-08-25 19:19:11 -0300

    Variaveis

---
+++ 02 - aula/variaveis.dart
@@ -0,0 +1,22 @@
+main() {
+  var hello = "Bem-vindo(a) a programação com Dart";
+
+  // hello = "Outro valor";
+  // hello = "Terceiro valor";
+
+  var numero = 2;
+
+  /*
+    note que Dart é fortemente tipada, ou seja,
+    não podemos realizar uma nova atribuição
+    a uma variável com um tipo diferente
+    do especificado no comando var.
+
+    numero = "texto"; // incorreto
+  */
+
+  // numero = 3;
+
+  print(hello);
+  print(numero);
+}

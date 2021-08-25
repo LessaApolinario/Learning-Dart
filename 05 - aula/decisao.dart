66d08de29b5dfbbb0351c7ee1b298e42b6bace75
Lessa Apolinario <lessaapolinario22@outlook.com>
2021-08-25 19:46:53 -0300

    if else

---
+++ 05 - aula/decisao.dart
@@ -0,0 +1,12 @@
+main() {
+  var num1 = 4;
+  var num2 = 4;
+
+  var isTrue = num1 >= num2; // true ou false
+
+  if (isTrue) {
+    print("Isso é verdadeiro");
+  } else {
+    print("Isso é falso");
+  }
+}

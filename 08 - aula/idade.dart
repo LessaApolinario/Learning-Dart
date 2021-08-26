0418292c227c36e97b4b2d7487a1870fd49c093b
Lessa Apolinario <lessaapolinario22@outlook.com>
2021-08-25 23:53:59 -0300

    Idades

---
+++ 08 - aula/idade.dart
@@ -0,0 +1,21 @@
+import 'dart:io'; // biblioteca para leitura de dados do teclado
+
+main() {
+  print("Digite uma idade: ");
+  var input = stdin.readLineSync();
+  var idade;
+
+  if (input != null) {
+    idade = int.parse(input);
+  }
+
+  if (idade > 50) {
+    print("Melhor idade");
+  } else if (idade >= 18) {
+    print("Adulto");
+  } else if (idade >= 12) {
+    print("Adolescente");
+  } else {
+    print("Criança");
+  }
+}

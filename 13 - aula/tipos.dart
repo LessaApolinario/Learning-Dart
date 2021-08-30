111f30a90c5dc43bc0df88f46854263cf5d8f020
Lessa Apolinario <lessaapolinario22@outlook.com>
2021-08-30 16:03:47 -0300

    Tipos principais

---
+++ 13 - aula/tipos.dart
@@ -0,0 +1,11 @@
+main() {
+  int num0 = 1;
+  double num1 = 1.2;
+  String texto = "some text";
+  bool isTrue = true; // true ou false
+
+  print(num0);
+  print(num1);
+  print(texto);
+  print(isTrue);
+}

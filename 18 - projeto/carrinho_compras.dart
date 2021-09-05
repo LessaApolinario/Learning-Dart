a2d8f55b5c1de19de251d847df27ea12997b906f
Lessa Apolinario <lessaapolinario22@outlook.com>
2021-09-04 22:35:12 -0300

    Programa modulado

--- 18 - projeto/carrinho_compras.dart
+++ 18 - projeto/carrinho_compras.dart
@@ -13,27 +13,32 @@ main() {
         print("Terminou o programa");
         condicao = false;
       } else if (text == "imprimir") {
-        for (var i = 0; i < produtos.length; i++) {
-          print("Item $i - ${produtos[i]}");
-        }
+        imprimir(produtos);
       } else if (text == "remover") {
-        print("Qual item deseja remover?");
-
-        for (var i = 0; i < produtos.length; i++) {
-          print("Item $i - ${produtos[i]}");
-        }
-
-        String? item = stdin.readLineSync();
-
-        if (item != null) {
-          produtos.removeAt(int.parse(item));
-
-          print("Item removido!");
-        }
+        remover(produtos);
       } else {
         produtos.add(text);
         print("\x1B[2J\x1B[0;0H"); // limpa a tela
       }
     }
+  }
+}
+
+void imprimir(List<String> produtos) {
+  for (var i = 0; i < produtos.length; i++) {
+    print("Item $i - ${produtos[i]}");
+  }
+}
+
+void remover(List<String> produtos) {
+  print("Qual item deseja remover?");
+  imprimir(produtos);
+
+  String? item = stdin.readLineSync();
+
+  if (item != null) {
+    produtos.removeAt(int.parse(item));
+
+    print("Item removido!");
   }
 }
